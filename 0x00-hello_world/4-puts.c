adbd3d51b957e59c3143a1631b4db069e5dcc4f3
Egwu Kenneth <egwukenneth69@gmail.com>
2023-07-13 16:34:56 -0700

    Commit task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-/* Prints an output on the screen using puts */
+/**
+ *  main - Prints an output on the screen using puts
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

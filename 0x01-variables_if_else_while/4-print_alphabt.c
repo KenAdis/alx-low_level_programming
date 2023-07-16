3b6629dec8df4a1cf69f3b6591860f24275ab602
Egwu Kenneth <egwukenneth69@gmail.com>
2023-07-16 06:56:35 -0700

    Commit task

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,11 +12,11 @@ int main(void)
 
 	while (lower <= 'z')
 	{
-		if (lower != 'q' || lower != 'e')
+		if (lower != 'q' && lower != 'e')
 		{
-			putchar(lower);
-			lower++;
+			putchar(lower);	
 		}
+		lower++;
 	}
 	putchar('\n');
 	return (0);

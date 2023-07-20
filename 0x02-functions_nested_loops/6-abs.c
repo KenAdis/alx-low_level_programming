d48e9ed7f43783a89d74a21f81a7cdcb3d220ab3
Egwu Kenneth <egwukenneth69@gmail.com>
2023-07-20 03:56:41 -0700

    Commit task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: input to the function
+ *
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}

0d1996d927113eecebf97800f5557a2cc27bb1d9
Egwu Kenneth <egwukenneth69@gmail.com>
2023-07-22 08:47:02 -0700

    Commit task

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,15 +11,15 @@ void print_line(int n)
 	if (n > 0)
 	{
 		int i;
+
 		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
+		_putchar('\n');
 	}
 	else
 	{
 		_putchar('\n');
 	}
-
-	_putchar('\n');
 }

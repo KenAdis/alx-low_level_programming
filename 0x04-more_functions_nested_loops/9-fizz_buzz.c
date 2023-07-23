93a454cb883520214138e4f85e44ac4ace9df10c
Egwu Kenneth <egwukenneth69@gmail.com>
2023-07-23 02:00:10 -0700

    Commit task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints out number 1 to 100 except for those divisible by 3, 5 & 15
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%i", i);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}

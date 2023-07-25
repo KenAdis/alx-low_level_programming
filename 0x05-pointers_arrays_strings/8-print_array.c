dcb1702e4e4726309b1469ce9c0fc6920aa28334
Egwu Kenneth <egwukenneth69@gmail.com>
2023-07-25 11:53:37 -0700

    Commit task

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: pointer to an int
+ * @n: number of elements of the array to be printed
+ *
+ * Return: Nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - 1)
+		{
+			printf("%d, ", *(a + i));
+		}
+		else
+		{
+			printf("%d\n", *(a + i));
+		}
+	}
+}

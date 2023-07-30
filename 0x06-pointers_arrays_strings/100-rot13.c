c1e0a53183148d9d441785ca8640735de6e925e7
Egwu Kenneth <egwukenneth69@gmail.com>
2023-07-30 04:26:30 -0700

    Commit task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+/**
+ * rot13 - rotates each alphabet by 13 characters
+ * @c: input string
+ *
+ * Return: returns the encodes string
+ */
+char *rot13(char *c)
+{
+	int i, j;
+	char *a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (c[i] == a[j])
+			{
+				c[i] = b[j];
+				break;
+			}
+		}
+	}
+
+	return (c);
+}

ec826064caab6613a9f950681986b9b8368346af
Egwu Kenneth <egwukenneth69@gmail.com>
2023-07-30 01:49:17 -0700

    Commit task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,7 @@ char *cap_string(char *str)
 			if (str[i] == n[j])
 			{
 				i++;
-				if (str[i] >= 97 || str[i] <= 122)
+				if (str[i] >= 97 && str[i] <= 122)
 				{
 					str[i] = str[i] - 32;
 				}

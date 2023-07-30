8dae6b8b7a9765f3244e9fcfbe1911db6cb60f7d
Egwu Kenneth <egwukenneth69@gmail.com>
2023-07-30 01:21:12 -0700

    Commit task

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+/**
+ * cap_string - changes all lowercase letters of a string to uppercase
+ * @str: input string
+ *
+ * Return: string in uppercase
+ */
+
+char *cap_string(char *str)
+{
+	int i, j;
+	char *n = ",;.!?(){}\n\t\" ";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (str[i] == n[j])
+			{
+				i++;
+				if (str[i] >= 97 || str[i] <= 122)
+				{
+					str[i] = str[i] - 32;
+				}
+			}
+		}
+	}
+
+	return (str);
+}
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,38 @@
+/**
+ * leet - encodes string
+ * @str: input string
+ *
+ * Return: encoded string
+ */
+
+char *leet(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == 'A' || str[i] == 'a')
+		{
+			str[i] = '4';
+		}
+		else if (str[i] == 'E' || str[i] == 'e')
+		{
+			str[i] = '3';
+		}
+		else if (str[i] == 'O' || str[i] == 'o')
+		{
+			str[i] = '0';
+		}
+		else if (str[i] == 'T' || str[i] == 't')
+		{
+			str[i] = '7';
+		}
+		else if (str[i] == 'L' || str[i] == 'l')
+		{
+			str[i] = '1';
+		}
+
+	}
+
+	return (str);
+}

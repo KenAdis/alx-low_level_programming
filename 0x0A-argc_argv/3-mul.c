d8c8cd7b73b16c530dc868ef73f555eb4b8458d6
Egwu Kenneth <egwukenneth69@gmail.com>
2023-08-09 16:12:31 -0700

    Commit Task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - prints the number of arguments passed into it
+ * main - multiplies two numbers
  * @argc: argument counter
  * @argv: argument vector
  *
@@ -10,15 +10,19 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
+	int num1, num2, mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
+	mul = num1 * num2;
 
-	if (argc < 3)
-		printf("Error\n");
-	else
-		printf("%d\n", num1 * num2);
+	printf("%d\n", mul);
 
 	return (0);
 }

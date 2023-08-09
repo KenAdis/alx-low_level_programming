6da20f1ef96eafe9c70fc4be326a7cd460bd77da
Egwu Kenneth <egwukenneth69@gmail.com>
2023-08-09 09:10:20 -0700

    Commit Task

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,5 +1,25 @@
-#include "main.h"
-
-char *_strncpy(char *dest, char *src, int n)
+/**
+ * _strcpy - copies the string pointed to by src, including the terminating
+ * null byte (\0), to the buffer pointed to by dest
+ * @dest: destination array
+ * @src: source array
+ *
+ * Return: string (array of characters)
+ */
+char *_strcpy(char *dest, char *src)
 {
+	int i, len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }

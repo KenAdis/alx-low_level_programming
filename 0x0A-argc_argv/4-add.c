a151e1c5345580fcfedb8bede1fea44c3da2a7a2
Egwu Kenneth <egwukenneth69@gmail.com>
2023-08-09 17:12:00 -0700

    Commit Task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, num, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (num = 0; argv[i][num]; num++)
+		{
+			if (argv[i][num] < '0' || argv[i][num] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
